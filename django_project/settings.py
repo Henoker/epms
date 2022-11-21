42c67f07fe68da1261e292736d1d1e61af0b3a1b
Henoker <hennybany@gmail.com>
2022-11-21 21:37:22 +0300

    chore: set up initial registration and login page

--- django_project/settings.py
+++ django_project/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
